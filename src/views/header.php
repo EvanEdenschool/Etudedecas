01c53bd74c8c94476fe0df45607e74fc9fc83c85
julienclementweb <68154514+julienclementweb@users.noreply.github.com>
2020-09-29 10:38:42 +0200

    Header OK

--- src/views/header.php
+++ src/views/header.php
@@ -1,15 +1,18 @@
-<div id="header" class="container-fluid">
+<div id="header" class="container">
     <div class="row">
-        <div class="col-md-4" id="search">  
+       <div class="col-md-6">
+            <h2>Mediastore</h2>
+            <a href="#">Home</a>
+            <a href="#">CD</a>
+            <a href="#">DVD</a>
+       </div>
+        <div class="col-md-3">
+            <div class="btn btn-danger">INSCRIPTION</div>
+            <div class="btn btn-danger">CONNEXION</div>
         </div>
-        <div class="col-md-4">
-            <h1>Medi@store</h1>
-        </div>
-        <div class="col-md-4" id="login">
-            <a href="#">Inscription</a> | <a href="#">Connexion</a>
-            <input type="search" name="search">
-            <button><span class="glyphicon glyphicon-search"></span></button>
+        <div class="col-md-3">
+            <button id="searchBtn"><span class="glyphicon glyphicon-search"></span></button>
+            <input type="search" id="site-search" name="search">
         </div>
     </div>
-
 </div>
