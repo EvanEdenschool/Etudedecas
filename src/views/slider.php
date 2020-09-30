7ad15c3a518dc7b00242242b85090e4db5473d02
julienclementweb <68154514+julienclementweb@users.noreply.github.com>
2020-09-30 16:24:26 +0200

    slider

--- src/views/slider.php
+++ src/views/slider.php
@@ -2,16 +2,35 @@
 
 </div>
 
+
 <div class="slide_inside container-fluid">
+
     <div id="slider" class="container">
-        <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
-            <div class="carousel-inner" role="listbox">
-                <?php 
-                for($i=0;$i<1;$i++) {?>
-                <div class="item active">
-                    <img src="./img/dvd2" alt="">
+        <div class="row">
+            <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+                <div class="carousel-inner" role="listbox">
+                    
+                    <ul class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                    </ul>
+
+                    <div class="item active">
+                        <img src="./img/dvd2" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="./img/dvd4" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="./img/dvd5" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="./img/dvd6" alt="">
+                    </div>
+
                 </div>
-                <?php } ?>
             </div>
         </div>
     </div>
