120f69660a5614ea6a31716fbc43a0ba84d8e13e
Evan <e.nourikyan@xefi.fr>
2020-10-01 10:56:06 +0200

    panier + routeur

--- src/views/header.php
+++ src/views/header.php
@@ -1,3 +1,11 @@
+<?php
+$linkToMonCompte = Controller::redirectTo('mon_compte.php');
+$linkToSeDeconnecter = Controller::redirectTo('se_deconnecter.php');
+$linkToInscription = Controller::redirectTo('inscription.php');
+$linkToLogin = Controller::redirectTo('login.php');
+$linkToPanier = Controller::redirectTo('panier.php');
+?>
+
 <div id="header" class="container">
     <div class="row">
        <div class="col-md-4">
@@ -8,19 +16,19 @@
        </div>
         <div class="col-md-4">
             <?php if (isset($_SESSION['user_id'])) { ?>
-                <a class="btn btn-danger" href="views/mon_compte.php?id=<?=$_SESSION['user_id']?>">MON COMPTE</a>
-                <a class="btn btn-danger" href="views/se_deconnecter.php?id=<?= $_SESSION['user_id'] ?>">DECONNEXION</a>
+                <a class="btn btn-danger" href="<?= $linkToMonCompte ?>?id=<?=$_SESSION['user_id']?>">MON COMPTE</a>
+                <a class="btn btn-danger" href="<?= $linkToSeDeconnecter ?>?id=<?= $_SESSION['user_id'] ?>">DECONNEXION</a>
 
             <?php } else { ?>
-                <a class="btn btn-danger" href="views/inscription.php">INSCRIPTION</a>
-                <a class="btn btn-danger" href="views/login.php">CONNEXION</a>
+                <a class="btn btn-danger" href="<?= $linkToInscription ?>">INSCRIPTION</a>
+                <a class="btn btn-danger" href="<?= $linkToLogin ?>">CONNEXION</a>
             <?php } ?>
         </div>
         <div class="col-md-4">
             <button id="searchBtn"><span class="glyphicon glyphicon-search"></span></button>
             <input type="search" id="site-search" name="search" placeholder="Rechercher un produit">
             <?php  if(isset($_SESSION['user_id'])) { ?>
-                <div class="btn btn-danger cart"><?=isset($_SESSION['panier']['count']) ? $_SESSION['panier']['count'] : ""?><span class="glyphicon glyphicon-shopping-cart"></span></div>
+                <a href="<?= $linkToPanier ?>" class="btn btn-danger cart"><?=isset($_SESSION['panier']['count']) ? $_SESSION['panier']['count'] : ""?><span class="glyphicon glyphicon-shopping-cart"></span></a>
             <?php } ?>
         </div>
     </div>
